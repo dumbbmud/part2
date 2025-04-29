578109c6994ed4418057e497217ccc9a1277da44
dumbbmud <ayeshamani63@gmail.com>
2025-04-28 19:48:16 -0500

    part 2.6

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,52 +1,107 @@
-import Curriculum from "./components/Curriculum"
+// import Curriculum from "./components/Curriculum"
+
+// const App = () => {
+//   const courses = [
+//     {
+//       name: 'Half Stack application development',
+//       id: 1,
+//       parts: [
+//         {
+//           name: 'Fundamentals of React',
+//           exercises: 10,
+//           id: 1
+//         },
+//         {
+//           name: 'Using props to pass data',
+//           exercises: 7,
+//           id: 2
+//         },
+//         {
+//           name: 'State of a component',
+//           exercises: 14,
+//           id: 3
+//         },
+//         {
+//           name: 'Redux',
+//           exercises: 11,
+//           id: 4
+//         }
+//       ]
+//     }, 
+//     {
+//       name: 'Node.js',
+//       id: 2,
+//       parts: [
+//         {
+//           name: 'Routing',
+//           exercises: 3,
+//           id: 1
+//         },
+//         {
+//           name: 'Middlewares',
+//           exercises: 7,
+//           id: 2
+//         }
+//       ]
+//     }
+//   ]
+
+//   return <Curriculum courses={courses} />
+// }
+
+// export default App
+
+import { useState } from 'react'
+
+const DisplayNames = ({persons}) =>{
+  return (
+    <div>{persons.name} </div>
+  )
+}
+
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
-  return <Curriculum courses={courses} />
+  const handleNewName = (event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    setPersons(persons.concat())
+  }
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+
+  }
+
+  console.log("array", persons)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNewName} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(x => 
+        <DisplayNames key={persons.indexOf(x)} persons={x} />
+      )}
+    </div>
+  )
 }
 
 export default App
